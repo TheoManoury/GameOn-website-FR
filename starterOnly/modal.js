096766539d937f3224e6736f46b31aaa9dc80d06
TheoManoury <theo@lovelace-balzac.com>
2022-09-27 17:05:52 +0200

    feat: close modal on close button click, fix #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,36 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav")
   if (x.className === "topnav") {
-    x.className += " responsive";
+    x.className += " responsive"
   } else {
-    x.className = "topnav";
+    x.className = "topnav"
   }
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalBg = document.querySelector(".bground")
+const modalBtn = document.querySelectorAll(".modal-btn")
+const formData = document.querySelectorAll(".formData")
+const closeModalBtn = document.getElementById("close-button")
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal))
+
+// close modal event
+closeModalBtn.addEventListener("click", closeModal)
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalBg.style.display = "block"
 }
 
-
+// close modal form
+function closeModal() {
+  modalBg.style.opacity = "0"
+  modalBg.style.transform = "translateY(-150px)"
+  setTimeout(() => {
+    modalBg.style.display = "none"
+    modalBg.style.removeProperty("opacity")
+    modalBg.style.removeProperty("transform")
+  }, 800)
+}
